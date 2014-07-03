c7f80391e6c3a1a638a18e08aa14c5d25968ace9
lukehayes <lukehayes.me@hotmail.co.uk>
2014-07-03 09:25:01 +0100

    Removed unneeded onditionals

--- index.php
+++ index.php
@@ -1,44 +1,32 @@
+<?php
+/**
+ * The main template file. 
+ * 
+ * Loads all of the different post types. If post type is standard, format.php runs.
+ */
+
+?>
+
 <?php get_header(); ?>
 
+<?php if(have_posts()) :?> 
+
+	<?php while(have_posts()) : the_post(); ?>
+
+		<?php 
+
+		/**
+		 *  Will try to find a format-POST_TYPE.php file, if not, will just run format.php (ie, standard post);
+		 */
+
+		?>
+
+		<?php get_template_part( 'format', get_post_format() ); ?>
 
 
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+	<?php endwhile; else : ?>
 
-	<?php $post_format = get_post_format(); ?>	
-
-
-	<?php if (has_post_format( $post_format)): ?>
-		
-		<?php get_template_part( 'format-'.$post_format ); ?>
-
-	<?php else: ?>
-
-		<?php get_template_part('format-standard'); ?>
-		
-	<?php endif ?>
-
-
-	<!-- Saves the need to use single.php files :) -->
-	<?php if (is_single()): ?>
-		
-		<div class="comments-area">
-			<?php comments_template(); ?>
-		</div>
-
-	<?php endif ?>
-
-
-<?php endwhile; else : ?>
-
-	<div class="post">
-		
-		<h2>Page Not Found</h2>
-		
-		<p>Looks like the page you're looking for isn't here anymore. Try browsing the <a href="">categories</a>, <a href="">archives</a>, or using the search box below.</p>
-		
-		<?php include(TEMPLATEPATH.'/searhform.php'); ?>
-		
-	</div> <!-- .post -->
+	<?php get_template_part('format', 'none'); ?>
 
 <?php endif; ?>
 
