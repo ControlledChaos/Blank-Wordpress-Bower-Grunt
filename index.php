66eb7fce4d88d7ab753873c04d88c171bbd511df
lukehayes <lukehayes.me@hotmail.co.uk>
2014-09-11 23:26:44 +0100

    Added main tag

--- index.php
+++ index.php
@@ -9,31 +9,32 @@
 
 <?php get_header(); ?>
 
-<?php if(have_posts()) :?> 
+<main id="main">
 
-	<?php while(have_posts()) : the_post(); ?>
+	<?php if(have_posts()) :?> 
 
-		<?php 
+		<?php while(have_posts()) : the_post(); ?>
 
-		/**
-		 *  Will try to find a format-POST_TYPE.php file, if not, will just run format.php (ie, standard post);
-		 */
+			<?php 
 
-		?>
+			/**
+			 *  Will try to find a format-POST_TYPE.php file, if not, will just run format.php (ie, standard post);
+			 */
 
-		<?php get_template_part( 'format', get_post_format() ); ?>
+			?>
+
+			<?php get_template_part( 'format', get_post_format() ); ?>
 
 
-	<?php endwhile; else : ?>
+		<?php endwhile; else : ?>
 
-	<?php get_template_part('format', 'none'); ?>
+		<?php get_template_part('format', 'none'); ?>
 
-<?php endif; ?>
+	<?php endif; ?>
 
-<div class="navigation clear">
-	<div class="left"><?php next_posts_link('&laquo; Older Entries') ?></div>
-	<div class="right"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-</div>
+	<?php post_nav_links(); ?>
+
+</main>
 
 
 
