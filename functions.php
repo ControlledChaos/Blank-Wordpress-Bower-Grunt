931b7ac3fc6492970c7e024cf25d574f1503d15d
lukehayes <lukehayes.me@hotmail.co.uk>
2014-09-12 09:37:37 +0100

    Rearranged file

--- functions.php
+++ functions.php
@@ -1,11 +1,107 @@
 <?php 
 
-/* A file for the functions */
+/**
+ * 
+ *  Functions for the theme
+ * 
+ * 	@package awesome
+ */
+
+
+/**
+ * Setup default parts of the theme (menues, post types etc)
+ */
+function tt_theme_setup() {
+
+	/**
+	 * Add Featured image support
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+
+
+	/**
+	 * Add post format support link for main post types
+	 */
+
+	add_theme_support( 'post-formats',array('image','quote','video','gallery','status','link') );
+
+
+
+	/**
+	 * Define Header/Main menu area
+	 */
+	register_nav_menu('header_menu','The menu for the header/main menu of the website.');
+
+
+	/**
+	 * Define Header/Main menu area
+	 */
+
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+}
+
+
+add_action('after_setup_theme', 'tt_theme_setup');
+
 
 
 /* ******************************************
-	Include all of the custom functionality
+	Load all Javascipt and CSS files
 	*********************************************/
+
+	function tt_theme_scripts() {
+
+		if (is_admin()) return;
+
+		/**
+		 *  Register jQuery
+		 */
+
+		wp_enqueue_script( 'jquery' );
+
+		/**
+		 *  Register and enqueue Bookstrap JS
+		 */
+		wp_register_script('bootstrap_js', get_template_directory_uri() . '/includes/js/sass-bootstrap.min.js', false, false , false );
+		wp_enqueue_script('bootstrap_js');
+
+
+		/**
+		 *  Register and enqueue Custom JS file
+		 */
+		wp_register_script( 'custom_js', get_template_directory_uri() . '/includes/js/custom.js', false, false, true );
+		wp_enqueue_script( 'custom_js' );
+
+
+
+		/**
+		 *  Register and enqueue Bookstrap CSS
+		 */
+		wp_register_style('bootstrap_css', get_template_directory_uri() . '/includes/stylesheets/bootstrap.min.css');
+		wp_enqueue_style('bootstrap_css');
+
+
+
+		/**
+		 *  Register and enqueue Custrom CSS
+		 */
+		wp_register_style('wp_default_stylesheet', get_stylesheet_uri() );
+		wp_enqueue_style('wp_default_stylesheet');
+
+	}
+
+
+	add_action('wp_enqueue_scripts','tt_theme_scripts');
+
+
+
+	/**
+	 * 
+	 * 	Include all of the custom functionality
+	 * 
+	 */
 
 
 	include('includes/themetacular/post_format_meta_boxes.php');
@@ -14,72 +110,6 @@
 	include('includes/themetacular/shortcodes.php');
 	include('includes/themetacular/template_tags.php');
 
-
-
-
-/* ******************************************
-	Load all Javascipt and CSS files
-	*********************************************/
-
-	add_action('wp_enqueue_scripts','themetacular_load_js'); 
-
-	function themetacular_load_js() {
-
-		if (is_admin()) return;
-
-	// // Load jQuery
-		wp_enqueue_script('jquery');
-
-	//Custom Boostrap JS
-		wp_register_script('bootstrap_js', get_template_directory_uri() . '/includes/js/sass-bootstrap.min.js', false, false , false );
-		wp_enqueue_script('bootstrap_js');
-
-	//Custom JS
-		wp_register_script( 'custom_js', get_template_directory_uri() . '/includes/js/custom.js', false, false, true );
-		wp_enqueue_script('custom_js');
-
-		wp_register_style('bootstrap_css', get_template_directory_uri() . '/includes/stylesheets/bootstrap.min.css');
-		wp_enqueue_style('bootstrap_css');
-
-		wp_register_style('wp_default_stylesheet', get_stylesheet_uri() );
-		wp_enqueue_style('wp_default_stylesheet');
-
-	}
-
-
-
-
-
-/* ******************************************
-	Define Header/Main menu area
-	*********************************************/
-
-	register_nav_menu('header_menu','The menu for the header/main menu of the website.');
-
-/* ******************************************
-	Register the apperaence menus
-	*********************************************/
-
-	// add_theme_support('menu');
-
-
-
-
-/* ******************************************
-	Add Featured Image Support
-	*********************************************/
-
-
-	add_theme_support( 'post-thumbnails' );
-
-
-
-
-/* ******************************************
-	Add Post Format Support
-	*********************************************/
-
-	add_theme_support( 'post-formats',array('image','quote','video','gallery','status','link') );
 
 
 
