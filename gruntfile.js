0a06578a691a4c5a0b3550621399b9494addf99d
lukehayes <lukehayes.me@hotmail.co.uk>
2014-09-22 16:08:58 +0100

    Removes unused modules

--- gruntfile.js
+++ gruntfile.js
@@ -27,38 +27,17 @@ module.exports = function(grunt) {
                 },
                 files: {
                     // 'includes/stylesheets/reset.css': 'includes/sass/reset.scss',
-                    'includes/stylesheets/screen.css': 'includes/sass/screen.scss',
-                    'includes/stylesheets/variables.css': 'includes/sass/variables.scss',
+                    'includes/stylesheets/main.css': 'includes/sass/main.scss',
                     'includes/stylesheets/typography.css': 'includes/sass/typography.scss',
                 }
             } 
-        },
-
-        // compass: {                  // Task
-        //     dist: {                   // Target
-        //       options: {              // Target options
-        //         sassDir: 'includes/sass',
-        //         cssDir: 'includes/stylesheets',
-        //         environment: 'production',
-        //       }
-        //     },
-        //     dev: {                    // Another target
-        //       options: {
-        //         sassDir: 'includes/sass',
-        //         cssDir: 'includes/stylesheets'
-        //       }
-        //     }
-        //   }
-
-
+        }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    // grunt.loadNpmTasks('grunt-contrib-compass');
-    // grunt.loadNpmTasks('grunt-contrib-concat');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('dev', ['watch','sass']);
