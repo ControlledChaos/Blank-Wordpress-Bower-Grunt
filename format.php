9414d5edc587bbc8fd7fb7b3ce3d4bd9baf6c796
lukehayes <lukehayes.me@hotmail.co.uk>
2014-09-22 15:34:30 +0100

    Tidied the file, added comments

--- format.php
+++ format.php
@@ -1,18 +1,31 @@
-<?php 
+<?php
 /**
-	This file is runs as the standard post format.
-*/
+ * This file is runs as the standard post format.
+ *
+ * @packaga
+ */
 ?>
+
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+	
+	<!-- Add the featured image if any -->
 	<?php get_template_part('format','post_thumbnail'); ?>
 
 	<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
 
-	<?php the_content(); ?>
+	<!-- Get our post meta -->
+	<?php get_template_part('format','meta'); ?>
+
+	<div class="entry">
+		
+		<?php the_content(); ?>
+		
+	</div>
 
 
+	<!-- Load the comments template -->
 	<?php load_comments_template(); ?>
 
 </article>
