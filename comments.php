13b4e779664af62219cde2ed3fc06b78dc9d3d0b
lukehayes <lukehayes.me@hotmail.co.uk>
2014-09-22 17:43:26 +0100

    changed the comments template

--- comments.php
+++ comments.php
@@ -1,90 +1,70 @@
-<?php // Do not delete these lines
-if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-	die ('Please do not load this page directly. Thanks!');
-	if (!empty($post->post_password)) { // if there's a password
-	if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) { ?>
-	<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
-	<?php return;
+<?php
+/**
+ * The template for displaying Comments.
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.
+ *
+ * @package Editor
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
 }
-}
-/* This variable is for alternating comment background */
-$oddcomment = 'class="alt" ';
 ?>
 
-<!-- Start editing here. -->
+<div id="comments" class="comments-area">
 
-<?php if ($comments) : ?>
-	<h3><?php comments_number('No Comments', 'One Comment', '% Comments' );?> on &#8220;<?php the_title(); ?>&#8221;</h3>
+	<?php // You can start editing here -- including this comment! ?>
 
-	<ul class="comment-list">
+	<?php if ( have_comments() ) : ?>
+		<h3 class="comments-title">
+			<?php
+				printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'themetacular' ),
+					number_format_i18n( get_comments_number() ), get_the_title() );
+			?>
+		</h3>
 
-		<?php foreach ($comments as $comment) : ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-above" class="comment-navigation" role="navigation">
+			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'themetacular' ); ?></h1>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'themetacular' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'themetacular' ) ); ?></div>
+		</nav><!-- #comment-nav-above -->
+		<?php endif; // check for comment navigation ?>
 
-			<li <?php echo $oddcomment; ?>id="comment-<?php comment_ID() ?>">
-				
-				<span class='comment-avatar'><?php echo get_avatar( $comment, 32 ); ?></span>
-				<span class='comment-author-link'><?php comment_author_link() ?></span>
-				<span class='comment-approval'><?php if ($comment->comment_approved == '0') : echo "<em>Your comment is awaiting moderation.</em>"; endif; ?></span>
+		<ol class="comment-list">
+			<?php
+				wp_list_comments( array(
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'avatar_size' => 50
+				) );
+			?>
+		</ol><!-- .comment-list -->
 
-				<span class='comment-date'><?php comment_date('F jS, Y') ?></span>
-				<span class='comment-time'><?php comment_time() ?></span>
-				<span class='comment-edit-link'><?php edit_comment_link('edit','&nbsp;&nbsp;',''); ?></span>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below" class="comment-navigation" role="navigation">
+			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'themetacular' ); ?></h1>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'themetacular' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'themetacular' ) ); ?></div>
+		</nav><!-- #comment-nav-below -->
+		<?php endif; // check for comment navigation ?>
 
-				<div class="comment-text">
-					<?php comment_text() ?>
-				</div>
+	<?php endif; // have_comments() ?>
 
-			</li>
-
-			<?php $oddcomment = ( empty( $oddcomment ) ) ? 'class="alt" ' : ''; ?>
-
-		<?php endforeach; ?>
-
-	</ul>
-
-<?php else : // this is displayed if there are no comments so far ?>
-
-	<?php if ('open' == $post->comment_status) : ?>
-
-	<?php else : ?>
-		<p class="nocomments">Comments are closed.</p>
-
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'themetacular' ); ?></p>
 	<?php endif; ?>
-<?php endif; ?>
 
+	<?php comment_form(); ?>
 
-<?php if ('open' == $post->comment_status) : ?>
-
-	<h3 class='leave-reply'>Leave a Reply</h3>
-
-	<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
-		<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p>
-
-	<?php else : ?>
-
-		<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="comment-form">
-
-			<?php if ( $user_ID ) : ?>
-
-				<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a></p>
-
-			<?php else : ?>
-
-				<p class="clearfix"><label for="author">Name <?php if ($req) echo "<span>*</span>"; ?></label> <input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" tabindex="1" /></p>
-
-				<p class="clearfix"><label for="email">Email<?php if ($req) echo "<span>*</span>"; ?></label> <input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" tabindex="2" /></p>
-
-			<?php endif; ?>
-
-			<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
-
-			<p class="clearfix"><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" /></p>
-			<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-
-			<p class="denotes">* Denotes required field.</p>
-
-			<?php do_action('comment_form', $post->ID); ?>
-
-		</form>
-
-	<?php endif; endif; ?>
+</div><!-- #comments -->
