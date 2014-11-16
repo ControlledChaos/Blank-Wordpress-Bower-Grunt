66695e0c4e52f452635ff1c60c1cbb616c3633cb
lukehayes <lukehayes.me@hotmail.co.uk>
2014-11-16 11:08:55 +0000

    Removed the bootstrap css

--- page.php
+++ page.php
@@ -1,29 +1,21 @@
 <?php get_header(); ?>
 
 
-<div class="container">
+
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 	
-	<div class="row">
-
-
-		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-			
-			<article class="post" id="<?php the_ID(); ?>">
-				
-				<h2><?php the_title(); ?></h2>
-				
-				<div class="entry">
-					<?php the_content(); ?>
-				</div>
-				
-			</article> <!-- .post -->
-
-		<?php endwhile; endif; ?>
+	<article class="post" id="<?php the_ID(); ?>">
 		
+		<h2><?php the_title(); ?></h2>
 		
+		<div class="entry">
+			<?php the_content(); ?>
+		</div>
+		
+	</article> <!-- .post -->
 
-	</div>
-</div>
+<?php endwhile; endif; ?>
+
 
 
 <?php get_sidebar(); ?>
