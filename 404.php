b701fc4d92ce71955841868aea6970d54ebd5fe8
lukehayes <lukehayes.me@hotmail.co.uk>
2014-12-05 09:25:54 +0000

    Added 404 p tag and post-header tag

--- 404.php
+++ 404.php
@@ -1,11 +1,20 @@
 <?php get_header(); ?>
 
-<article class="post">
+<article class="page post">
+
+	<header class="post-header">
+
+		<p>404 :'(</p>
+
+		<h2>Oops something has gone wrong.</h2>
+
+		<p class="404-description">
+			Something appears to have gone wrong - perhaps the page you were looking for does not exist anymore?
+		</p>
+
+
+	</header>
 	
-	<h2>Oops something has gone wrong.</h2>
-	<p class="404-description">
-		Something appears to have gone wrong - perhaps the page you were looking for does not exist anymore?
-	</p>
 	
 	<?php include(TEMPLATEPATH.'/searhform.php'); ?>
 	
