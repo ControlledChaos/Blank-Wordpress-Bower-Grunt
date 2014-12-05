2515fb925ec4a5837741090f4d76f408c517cf3c
lukehayes <lukehayes.me@hotmail.co.uk>
2014-12-05 09:34:52 +0000

    Removed search bar, added archive list

--- 404.php
+++ 404.php
@@ -1,24 +1,40 @@
 <?php get_header(); ?>
 
-<article class="page post">
 
-	<header class="post-header">
+<main id="main" role="main">
+	<article class="page post">
 
-		<p>404 :'(</p>
+		<header class="post-header">
 
-		<h2>Oops something has gone wrong.</h2>
+			<p>404 :'(</p>
 
-		<p class="404-description">
-			Something appears to have gone wrong - perhaps the page you were looking for does not exist anymore?
-		</p>
+			<h2>Oops something has gone wrong.</h2>
+
+			<p class="404-description">
+				Something appears to have gone wrong - perhaps the page you were looking for does not exist anymore?
+			</p>
 
 
-	</header>
-	
-	
-	<?php include(TEMPLATEPATH.'/searhform.php'); ?>
-	
-</article> <!-- .post -->
+		</header>
+
+		<div class="entry">
+			<?php 
+
+			/**
+			 *  Display a list of articles that have been created
+			 * 	in the last month for the user
+			 */
+				$args = array(
+					'type' = 'montly',
+					'format' = 'html'
+					);
+
+				wp_get_archives($args);
+			?>
+		</div>
+
+	</article> <!-- .post -->
+</main>
 
 
 
