528f0ea4629fe762878cff6a6d134548ebeb7b8d
lukehayes <lukehayes.me@hotmail.co.uk>
2014-12-05 09:41:48 +0000

    Added comments and post header

--- format-image.php
+++ format-image.php
@@ -1,17 +1,30 @@
-
-<!-- image post format file	 -->
-
+<?php
+/**
+ * This file is runs as the image post format.
+ *
+ * @package
+ */
+?>
 
 <article id="post-<?php the_ID(); ?>"  <?php post_class(); ?>>
 
+	<!-- Get our featured image -->
 	<?php get_template_part('format','post_thumbnail'); ?>
+
+	<header class="post-header">
+		<h1>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h1>
+	</header>
 	
-	<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 	
 	<div class="image-entry entry">
 		<?php the_content(); ?>
 	</div>
 
+	<!-- Load the comments template if single -->
 	<?php load_comment_template(); ?>
 	
 	
