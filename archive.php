dc5cb99713f4b23583ec8f7fb36c932567aee0f6
lukehayes <lukehayes.me@hotmail.co.uk>
2014-12-06 14:13:49 +0000

    Added get template part to be more DRY

--- archive.php
+++ archive.php
@@ -68,15 +68,7 @@
 
 	<?php endwhile; else : ?>
 
-	<div class="post">
-		
-		<h2>Page Not Found</h2>
-		
-		<p>Looks like the page you're looking for isn't here anymore. Try browsing the <a href="">categories</a>, <a href="">archives</a>, or using the search box below.</p>
-		
-		<?php include(TEMPLATEPATH.'/searhform.php'); ?>
-		
-	</div> <!-- .post -->
+		<?php get_template_part('format', 'none') ?>
 
 <?php endif; ?>
 
