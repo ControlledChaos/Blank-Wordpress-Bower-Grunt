1a0fdba3594178ab94c026921010b32a22bd39c6
lukehayes <lukehayes.me@hotmail.co.uk>
2014-12-06 13:54:03 +0000

    Added comments and header

--- format-status.php
+++ format-status.php
@@ -6,13 +6,25 @@
 
 	<?php get_template_part('format','post_thumbnail'); ?>
 	
-	<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+	<!-- Wrap the title etc in a header for html5 goodness -->
+	<header class="post-header">
+		<h1>
+			<a href="<?php the_permalink() ?>">
+				<?php the_title(); ?>
+			</a>
+		</h1>
+	</header>
+
 	
+	<!-- Get the content for out post -->
 	<div class="status-entry entry">
 		<?php the_content(); ?>
 	</div>
 
-	<?php load_comment_template(); ?>
+
+	<!-- Load the comments template -->
+	<?php themetacular_load_comments_template(); ?>
 
 	
 </article> <!-- .post -->
