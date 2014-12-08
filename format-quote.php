646b212f81815a4df37b166df3a205e6314875d1
lukehayes <lukehayes.me@hotmail.co.uk>
2014-12-08 09:41:40 +0000

    Organised the file and added comments

--- format-quote.php
+++ format-quote.php
@@ -1,18 +1,31 @@
-
-<!-- quote post format file	 -->
-
+<?php
+/**
+ * This file is runs as the Quote post format.
+ *
+ * @package
+ */
+?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+	<!-- Get the featured image of the post -->
 	<?php get_template_part('format','post_thumbnail'); ?>
 	
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	
+	<!-- Wrap the title in a header tag -->
+	<header class="post-header">
+		<h2>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h2>	
+	</header>
+
+	<!-- The content of our post -->
 	<div class="quote-entry entry">
 		<?php the_content(); ?>
 	</div>
 
+	<!-- Get the comments of our post type if we are in single -->
 	<?php load_comment_template(); ?>
-	
 	
 </article> <!-- .post -->
