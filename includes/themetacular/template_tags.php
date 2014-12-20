c2bdd79190908fe11a5f75c8e09d0de631f06b56
lukehayes <lukehayes.me@hotmail.co.uk>
2014-12-20 17:32:49 +0000

    Moved the nav menu function into the tags file

--- includes/themetacular/template_tags.php
+++ includes/themetacular/template_tags.php
@@ -103,5 +103,33 @@ if( !function_exists( 'themetacular_the_content' )) {
 }
 
 
+/************************************************************
+ *  Change the output of wordpress menu i.e
+ * 	remove the ul and li tags and replace with simple
+ * 	HTML 5 nav and a tags
+************************************************************/
+if ( !function_exists('themetacular_nav_menu')) {
+
+	function themetacular_nav_menu( $menu, $allowed_tags ) {
+
+
+		$args = array(
+
+			'theme_location' => $menu,
+			'container'       => 'nav',
+			'container_id' => 'navigation',
+			'echo'            => false,
+			'items_wrap'      => '%3$s',
+			'depth'           => 0
+		);
+
+
+		echo strip_tags( wp_nav_menu( $args ), $allowed_tags);
+
+
+	}
+}
+
+
 
 ?>
