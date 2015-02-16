a7e6939554f0b8fa00572cbfa7959916fd092ea6
lukehayes <lukehayes.me@hotmail.co.uk>
2015-02-16 18:13:04 +0000

    Added an init file for framework

---
+++ includes/themetacular/themetacular_init.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * Require all of the files that the themetacular framework needs
+ * 
+ * @package awesome
+ */
+
+/************************************************************
+*  Tags that make current wp functions easier/better
+************************************************************/
+require('includes/themetacular/template_tags.php');
+
+/************************************************************
+*  Themetacular shortcodes
+************************************************************/
+require('includes/themetacular/shortcodes.php');
+
+
+/************************************************************
+*   Include all of the custom functionality
+************************************************************/
+require('includes/themetacular/themetacular_customizer.php');
+
+
