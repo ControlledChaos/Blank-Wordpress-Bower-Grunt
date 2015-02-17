265f53946e8727c778e3f1149bfe038f2481132f
lukehayes <lukehayes.me@hotmail.co.uk>
2015-02-17 17:58:17 +0000

    Added a header and more useful comments to the page.php

--- index.php
+++ index.php
@@ -3,44 +3,46 @@
  * The main template file. 
  * 
  * Loads all of the different post types. If post type is standard, format.php runs.
+ * 
+ * @package awesome
  */
 
 ?>
 
 <?php get_header(); ?>
 
+<!-- This div for the blog/archive/single section of the theme -->
 <div class="pure-g">
 	<div class="pure-u-1 pure-u-md-1">
 
+		<!-- Hold all of our posts in the main tag -->
 		<main id="main">
 
+			<!-- Do we have any posts? -->
 			<?php if(have_posts()) :?> 
 
+				<!-- If so, loop around all of them and display them according to its format type -->
 				<?php while(have_posts()) : the_post(); ?>
 
-					<?php 
-			/**
-			 *  Will try to find a format-POST_TYPE.php file, if not, will just run format.php (ie, standard post);
-			 */
-			?>
-			
-			<!-- Get our the posts that are available -->
-			<?php get_template_part( 'format', get_post_format() ); ?>
+					<!-- Will try to find a format-POST_TYPE.php file, if not, will just run format.php (ie, standard post); -->
 
-		<?php endwhile; else : ?>
-		
-		<!-- If there isn't any posts, load this one -->
-		<?php get_template_part('format', 'none'); ?>
+					<!-- Get our the posts that are available -->
+					<?php get_template_part( 'format', get_post_format() ); ?>
 
-	<?php endif; ?>
+				<?php endwhile; else : ?>
 
-	<?php post_nav_links(); ?>
+				<!-- If there isn't any posts, load this one -->
+				<?php get_template_part('format', 'none'); ?>
 
-</main>
+			<?php endif; ?>
+
+			<?php post_nav_links(); ?>
+
+		</main>
 
 
 
-</div><!-- 	pure-u-md-1 -->
+	</div><!-- 	pure-u-md-1 -->
 </div> <!-- pure-g -->
 
 
--- page.php
+++ page.php
@@ -1,15 +1,29 @@
+<?php
+/**
+ * This file is runs as the page post type
+ *
+ * @package awesome
+ */
+?>
+
+
 <?php get_header(); ?>
 
-
-
+<!-- If we have posts, loop through and show them -->
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 	
+	<!-- Wrap our post in a lovely article tag -->
 	<article class="post" id="<?php the_ID(); ?>">
-		
-		<h2><?php the_title(); ?></h2>
+
+		<!-- Wrap the title etc in a header for html5 goodness -->
+		<header class="post-header">
+			<h1>
+				<?php the_title(); ?>
+			</h1>
+		</header>
 		
 		<div class="entry">
-			<?php the_content(); ?>
+			<?php themetacular_the_content(); ?>
 		</div>
 		
 	</article> <!-- .post -->
