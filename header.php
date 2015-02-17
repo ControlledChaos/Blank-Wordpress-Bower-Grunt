4ecfbd618e9ef164109c48187fd2e0bf920f2ab2
lukehayes <lukehayes.me@hotmail.co.uk>
2015-02-17 17:31:41 +0000

    Added useful comments

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
+    <!-- Title of our website -->
 	<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
 
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
@@ -11,9 +11,12 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 	<?php wp_head(); ?>
-
 </head>
+<!-- body_class() injects classes that are built in to Wordpress -->
 <body <?php body_class(); ?> >
+
+<!-- Wrap the entire front end and a wrapper -->
+<div id="site-wrapper">
 
 <header>
 	<h1><?php bloginfo('name'); ?></h1>	
