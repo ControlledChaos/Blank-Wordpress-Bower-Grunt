913be737de74c820621cad7637811023478f63ac
lukehayes <lukehayes.me@hotmail.co.uk>
2015-02-17 18:19:40 +0000

    Added main tag to page.php

--- page.php
+++ page.php
@@ -13,28 +13,30 @@
 
 <div class="page-g">
 	<div class="pure-u-md-1">
-		
-		<!-- If we have posts, loop through and show them -->
-		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-			<!-- Wrap our post in a lovely article tag -->
-			<article class="post" id="<?php the_ID(); ?>">
+		<main id="main">
+			<!-- If we have posts, loop through and show them -->
+			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-				<!-- Wrap the title etc in a header for html5 goodness -->
-				<header class="post-header">
-					<h1>
-						<?php the_title(); ?>
-					</h1>
-				</header>
+				<!-- Wrap our post in a lovely article tag -->
+				<article class="post" id="<?php the_ID(); ?>">
 
-				<div class="entry">
-					<?php themetacular_the_content(); ?>
-				</div>
+					<!-- Wrap the title etc in a header for html5 goodness -->
+					<header class="post-header">
+						<h1>
+							<?php the_title(); ?>
+						</h1>
+					</header>
 
-			</article> <!-- .post -->
+					<div class="entry">
+						<?php themetacular_the_content(); ?>
+					</div>
 
-		<?php endwhile; endif; ?>
+				</article> <!-- .post -->
 
+			<?php endwhile; endif; ?>
+
+		</main>
 	</div>  <!-- pure-g -->
 </div><!--  pure-u-md-1 -->
 
