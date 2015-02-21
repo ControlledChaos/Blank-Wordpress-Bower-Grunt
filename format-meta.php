3c8a316934caf602f247b3d3482349211ea736ba
lukehayes <lukehayes.me@hotmail.co.uk>
2015-02-21 15:26:22 +0000

    Renamed entry-meta to post-meta

--- format-meta.php
+++ format-meta.php
@@ -7,27 +7,27 @@
 ?>
 
 
-<div class="entry-meta">
+<div class="post-meta">
 
 	<ul class='meta-list'>
 
 		<!-- If there are categories, display them -->
 		<?php if( has_category() ) { ?>
 		
-		<li><?php the_category( ', '); ?></li>
+		<li>Category: <?php the_category( ', '); ?></li>
 
 		<?php } ?>
 
 		<!-- If there are tags, display them -->
 		<?php if( has_tag() ) { ?>
 		
-		<li><?php the_tags( '' ); ?></li>
+		<li>Tags: <?php the_tags( '' ); ?></li>
 
 		<?php } ?>
 
 		<!-- If there are Comments, display the amount of them -->
 		
-		<li><?php comments_popup_link('0', '1', '%'); ?></li>
+		<li>Comments: <?php comments_popup_link('0', '1', '%'); ?></li>
 
 	</ul>
 	
